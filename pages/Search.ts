5097d0a3ea581220a99decb6a9721b3c25faa338
Anastasiia Mishchenko <51370779+anastasia-mishchenko@users.noreply.github.com>
2025-11-13 19:33:50 +0200

    [L3][HW] Add Search class

---
+++ pages/Search.ts
@@ -0,0 +1,43 @@
+import { expect, Locator, Page } from "@playwright/test";
+
+export class Search {
+  // Page elements
+  readonly page: Page;
+
+  // Search input
+  readonly searchInput: Locator;
+
+  // Predictive search
+  readonly predictiveSearch: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.searchInput = page.locator("#Search-In-Inline");
+    this.predictiveSearch = page.locator("div[data-predictive-search]").nth(0);
+  }
+
+  async verifySearchInputIsVisible() {
+    await expect(this.searchInput).toBeVisible();
+  }
+
+  async verifySearchInputHasCorrectPlaceholder() {
+    await expect(this.searchInput).toHaveAttribute("placeholder", "Search");
+  }
+  async verifyPredictiveSearchIsVisible() {
+    await expect(this.predictiveSearch).toBeVisible({ timeout: 5000 });
+  }
+  async verifyPredictiveSearchContainsSearchTerm(searchTerm: string) {
+    await expect(
+      this.predictiveSearch.locator('a[role="option"]')
+    ).toContainText(searchTerm, { timeout: 5000 });
+  }
+
+  async submitSearch(searchTerm: string) {
+    await this.searchInput.fill(searchTerm);
+    await this.searchInput.press("Enter");
+  }
+  async verifySearchInputHasCorrectValue(value: string) {
+    await this.searchInput.fill(value);
+    await expect(this.searchInput).toHaveValue(value);
+  }
+}
